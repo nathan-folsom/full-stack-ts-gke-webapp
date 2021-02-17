f172f93e0de07192795c317e759ea30b10e0e2b4
Nathan Folsom <n8folsom@gmail.com>
2021-02-16 16:15:01 -0800

    adding prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,15 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ServeStaticModule} from "@nestjs/serve-static";
+import {GraphQLModule} from '@nestjs/graphql';
 import {join} from 'path';
+import {PrismaService} from "./prisma.service";
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'web-ui/dist/web-ui')
+    }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
     })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+      AppService,
+      PrismaService
+  ],
 })
 export class AppModule {}
