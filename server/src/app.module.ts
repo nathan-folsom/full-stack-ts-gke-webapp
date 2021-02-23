7a5ecb8e5cc94a0ee78fa013f35a30eeed98a7d3
Nathan Folsom <n8folsom@gmail.com>
2021-02-22 23:12:09 -0800

    remove unused controllers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,36 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppService} from './app.service';
 import {ServeStaticModule} from "@nestjs/serve-static";
 import {GraphQLModule} from '@nestjs/graphql';
 import {join} from 'path';
 import {PrismaService} from "./db/prisma.service";
-import {UserController} from "./user/user.controller";
 import {UserService} from "./user/user.service";
 import {UserResolver} from "./user/user.resolver";
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'web-ui/dist/web-ui')
-    }),
-    GraphQLModule.forRoot({
-      typePaths: ['./**/*.graphql'],
-      definitions: {
-        path: join(process.cwd(), 'src/db/graphql.ts'),
-      },
-        path: '/api'
-    })
-  ],
-  controllers: [
-      AppController,
-      UserController
-  ],
-  providers: [
-      AppService,
-      PrismaService,
-      UserService,
-      UserResolver
-  ],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '../..', 'web-ui/dist/web-ui')
+        }),
+        GraphQLModule.forRoot({
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/db/graphql.ts'),
+            },
+            path: '/api',
+            cors: true
+        })
+    ],
+    providers: [
+        AppService,
+        PrismaService,
+        UserService,
+        UserResolver
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
