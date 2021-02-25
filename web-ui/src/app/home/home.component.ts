b875d8d4f1fff3f2cef6785b550cc66f435e1393
Nathan Folsom <n8folsom@gmail.com>
2021-02-24 18:01:38 -0800

    Home page styling/layout

--- web-ui/src/app/home/home.component.ts
+++ web-ui/src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import {ApolloQueryResult} from "@apollo/client/core";
 import {User} from "../model/user";
 import {Observable} from "rxjs";
 
+const defaultAccountInputs = {username: '', password: '', confirmPassword: ''};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,22 +13,35 @@ import {Observable} from "rxjs";
 })
 export class HomeComponent implements OnInit {
   user$: Observable<ApolloQueryResult<{ user: User }>>
-  accountInputs = {username: '', password: ''};
+  createAccountInputs;
+  loginInputs;
+  passwordError = '';
 
   constructor(private service: AuthService) { }
 
   ngOnInit(): void {
-    this.user$ = this.service.getUser().valueChanges
+    this.user$ = this.service.getUser().valueChanges;
+    this.resetInputs()
   }
 
   createAccount = () => {
-    this.service.createUser(this.accountInputs.username, this.accountInputs.password);
-    this.accountInputs = {username: '', password: ''};
+    if (this.createAccountInputs.confirmPassword === this.createAccountInputs.password) {
+      this.service.createUser(this.createAccountInputs.username, this.createAccountInputs.password);
+      this.createAccountInputs = defaultAccountInputs;
+      this.passwordError = '';
+    } else {
+      this.passwordError = 'Passwords do not match!';
+    }
   }
 
   logIn = () => {
-    this.service.logIn(this.accountInputs.username, this.accountInputs.password);
-    this.accountInputs = {username: '', password: ''};
+    this.service.logIn(this.loginInputs.username, this.loginInputs.password);
+    this.loginInputs = defaultAccountInputs;
+  }
+
+  resetInputs = () => {
+    this.createAccountInputs = {...defaultAccountInputs};
+    this.loginInputs = {...defaultAccountInputs};
   }
 
   logout = () => {
