d6d306bf0059740b07c5fa99693c7c0e6c866754
Nathan Folsom <n8folsom@gmail.com>
2021-03-09 15:42:59 -0800

    Add usernames to reservations frontend

--- web-ui/src/app/data-services/reservation/gql.ts
+++ web-ui/src/app/data-services/reservation/gql.ts
@@ -4,6 +4,7 @@ export const CREATE_RESERVATION = gql`
   mutation($reservation: CreateReservationInput!) {
     createReservation(reservation: $reservation) {
       id
+      username
       location
       time
       message
@@ -13,31 +14,33 @@ export const CREATE_RESERVATION = gql`
 `
 
 export const GET_ALL_RESERVATIONS = gql`
-    query {
-      reservations {
-        id
-        userId
-        location
-        message
-        time
-      }
+  query {
+    reservations {
+      id
+      username
+      userId
+      location
+      message
+      time
     }
+  }
 `
 
 export const FETCH_RESERVATIONS = gql`
-    query {
-      allReservations {
-        id
-        userId
-        location
-        time
-        message
-      }
+  query {
+    allReservations {
+      id
+      username
+      userId
+      location
+      time
+      message
     }
+  }
 `
 
 export const DELETE_RESERVATION = gql`
-    mutation ($reservationId: String!) {
-      deleteReservation(reservationId: $reservationId)
-    }
+  mutation ($reservationId: String!) {
+    deleteReservation(reservationId: $reservationId)
+  }
 `
